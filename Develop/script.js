e3f13d4880c826b208ef574e9710fbf8a78af9f5
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-03-02 11:19:09 +0300

    created arrays to hold password characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,30 @@
 // Assignment code here
+//global references
+var passwordLength = userPasswordInput() 
 
+function userPasswordInput (){
+  debugger;
+  var desiredLength = window.prompt ("How many digits does your password require?");
+  desiredLength = parseInt(desiredLength);
+  
+  if (desiredLength <= 8 || desiredLength >= 128) {
+    window.alert ("Try again");
+  return userPasswordInput();
+  } else {
+    return desiredLength;
+  }
+}
+
+// arrays holding values for password
+var lowerAlphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+ "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upperAlphabetArray = lowerAlphabetArray[3].toUpperCase();
+
+var charArray = ['!','@','#','$','%','&','*']
+
+
+console.log(userPasswordLength);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -7,7 +32,11 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); 
+
+
+
+
 
   passwordText.value = password;
 
@@ -15,3 +44,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+userPasswordLength();

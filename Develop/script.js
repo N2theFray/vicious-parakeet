9b9457cc49e453e01bbbe9319b2fe31a5d63f659
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-03-02 12:52:33 +0300

    added random variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,66 @@
 // Assignment code here
 //global references
-var passwordLength = userPasswordInput() 
+var randomLowerLetter = function (){
+  var value = Math.floor(Math.random()*26+1);
+  return value;
+}
 
-function userPasswordInput (){
-  debugger;
-  var desiredLength = window.prompt ("How many digits does your password require?");
-  desiredLength = parseInt(desiredLength);
-  
-  if (desiredLength <= 8 || desiredLength >= 128) {
-    window.alert ("Try again");
-  return userPasswordInput();
-  } else {
-    return desiredLength;
-  }
+var randomUpperLetter = function (){
+  var value = Math.floor(Math.random()*26+1);
+  return value;
+}
+
+var randomCharacter =function (){
+  var value = Math.floor(Math.random()*7+1);
+  return value;
 }
 
 // arrays holding values for password
 var lowerAlphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
  "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var upperAlphabetArray = lowerAlphabetArray[3].toUpperCase();
+var upperAlphabetArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var charArray = ['!','@','#','$','%','&','*']
+var charArray = ['!','@','#','$','%','&','*'];
+ console.log(upperAlphabetArray[3]);
 
-
-console.log(userPasswordLength);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  var passwordLength = userPasswordInput() 
+
+  // function to determine length of password 
+    function userPasswordInput (){
+      
+      var desiredLength = window.prompt ("How many digits does your password require?");
+      desiredLength = parseInt(desiredLength);
+      
+      if (desiredLength <=1 || desiredLength >= 128) {
+        window.alert ("Try again");
+      return userPasswordInput();
+      } else {
+        return desiredLength;
+      }
+    }
+
+  var generatePassword = function () {
+    //debugger;
+    var passwordHolder = [];
+   
+    for (var i = 0; i < passwordLength; i++){
+      passwordHolder[i]=lowerAlphabetArray[i];
+    }
+
+    // Turn passwordHolder into a string
+    var passwordToString = passwordHolder.join('');
+    return passwordToString;
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); 
 
@@ -45,4 +75,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-userPasswordLength();
+
